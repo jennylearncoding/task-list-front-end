38dfa5999700f5dc34c79ad85c6eaf1d8500f136
jenny xu <jingwenxu13@gmail.com>
2025-06-16 11:07:16 -0700

    local change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import TASKS from '../data.js';
+import { useState } from 'react';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
 
 const App = () => {
-  return (
+  return (git
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
